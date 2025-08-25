517271553dcd6138c317b47808f7c2709b8bdc7b
Nikhilsingh9889 <Nikhilsingh2002nik@gmail.com>
2025-08-25 15:51:43 +0530

    Delete vars /Jenkinsfile

--- vars/clone.groovy
+++
@@ -1,6 +0,0 @@
-def call(String url, String branch){
-echo "This is cloning the code"
-git url: 'https://github.com/Nikhilsingh9889/nginx-docker.git', branch: 'main'
-echo "Code Cloning successful"
-
-}
