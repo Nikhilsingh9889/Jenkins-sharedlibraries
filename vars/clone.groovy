6fd2275f6bf006ba2f765cb9bc0bdf4b99115979
Nikhilsingh9889 <Nikhilsingh2002nik@gmail.com>
2025-08-25 14:45:32 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,6 @@
+def call(String url, String branch){
+echo "This is cloning the code"
+git url: 'https://github.com/Nikhilsingh9889/nginx-docker.git', branch: 'main'
+echo "Code Cloning successful"
+
+}
